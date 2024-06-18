2a21d85a13905fa5b00719b91a6541dbaa3becc6
shunsuke-luxiar <shunsuke.tsukagoshi@luxiar.com>
2024-06-18 09:31:02 +0900

    管理者のseed作成とdeviseのcontroller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(
+  first_name: 'ad',
+  last_name: 'min',
+  email: 'admin@example.com',
+  password: 'password',
+)
