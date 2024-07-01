da86dfe9ef0a42ca989f2b195dc6b1b87da22e40
shunsuke-luxiar <shunsuke.tsukagoshi@luxiar.com>
2024-07-01 10:41:47 +0900

    labelの紐付けと一覧へのリンク

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def admin_edit_other_user?(user)
     current_user.admin? && current_user != user
   end
+
+  def index_path
+    case controller_name
+    when 'daily_reports'
+      daily_reports_path
+    when 'users'
+      users_path
+    else
+      root_path
+    end
+  end
 end
