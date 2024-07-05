3963552852d862ee95130001088f0bbced0bf4cb
shunsuke-luxiar <shunsuke.tsukagoshi@luxiar.com>
2024-07-05 15:14:50 +0900

    コメント投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :daily_reports do
     resource :thumbs_up, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
   resources :users
   devise_for :user
