3434e4017207009c043c80fd8b34140f54d89f33
shunsuke-luxiar <shunsuke.tsukagoshi@luxiar.com>
2024-07-08 10:18:45 +0900

    turbo-framesの使い方の修正

--- app/controllers/thumbs_ups_controller.rb
+++ app/controllers/thumbs_ups_controller.rb
@@ -3,14 +3,12 @@ class ThumbsUpsController < ApplicationController
 
   def create
     @daily_report.liked_users << current_user
-    params[:thumbs_up] = true
-    redirect_to daily_report_path(@daily_report)
+    render partial: 'daily_reports/thumbs_up', locals: { daily_report: @daily_report }
   end
 
   def destroy
     @daily_report.liked_users.destroy(current_user)
-    params[:thumbs_up] = true
-    redirect_to daily_report_path(@daily_report)
+    render partial: 'daily_reports/thumbs_up', locals: { daily_report: @daily_report }
   end
 
   private
