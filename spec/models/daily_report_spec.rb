9a337ddf1a8ff5051b1a74a5db412622dfb38b3d
shunsuke-luxiar <shunsuke.tsukagoshi@luxiar.com>
2024-07-10 16:45:12 +0900

    余分な記述を削除

--- spec/models/daily_report_spec.rb
+++ spec/models/daily_report_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe DailyReport, type: :model do
   describe 'titleのバリデーション' do
     context '適切な値のとき' do
       it '有効' do
-        daily_report = FactoryBot.build :daily_report, title: 'title'
+        daily_report = build :daily_report, title: 'title'
         expect(daily_report).to be_valid
       end
     end
 
     context 'titleがnilのとき' do
       it '無効' do
-        daily_report = FactoryBot.build :daily_report, title: nil
+        daily_report = build :daily_report, title: nil
         expect(daily_report).not_to be_valid
       end
 
       it 'エラーメッセージが"を入力してください"になる' do
-        daily_report = FactoryBot.build :daily_report, title: nil
+        daily_report = build :daily_report, title: nil
         daily_report.valid?
         expect(daily_report.errors[:title]).to include('を入力してください')
       end
@@ -25,41 +25,41 @@ RSpec.describe DailyReport, type: :model do
 
   describe '#mood_ja' do
     it 'moodがnormalなら"まあまあ"を返す' do
-      daily_report = FactoryBot.build :daily_report, mood: :normal
+      daily_report = build :daily_report, mood: :normal
       expect(daily_report.mood_ja).to eq 'まあまあ'
     end
 
     it 'moodがfunなら"楽しかった"を返す' do
-      daily_report = FactoryBot.build :daily_report, mood: :fun
+      daily_report = build :daily_report, mood: :fun
       expect(daily_report.mood_ja).to eq '楽しかった'
     end
 
     it 'moodがproductiveなら"捗った"を返す' do
-      daily_report = FactoryBot.build :daily_report, mood: :productive
+      daily_report = build :daily_report, mood: :productive
       expect(daily_report.mood_ja).to eq '捗った'
     end
 
     it 'moodがsleepyなら"眠かった"を返す' do
-      daily_report = FactoryBot.build :daily_report, mood: :sleepy
+      daily_report = build :daily_report, mood: :sleepy
       expect(daily_report.mood_ja).to eq '眠かった'
     end
 
     it 'moodがunwellなら"体調不良"を返す' do
-      daily_report = FactoryBot.build :daily_report, mood: :unwell
+      daily_report = build :daily_report, mood: :unwell
       expect(daily_report.mood_ja).to eq '体調不良'
     end
   end
 
   describe '#learned_tags_str' do
     it 'learned_tagを持たないとき空文字を返す' do
-      daily_report = FactoryBot.build :daily_report
+      daily_report = build :daily_report
       expect(daily_report.learned_tags_str).to eq ''
     end
 
     it 'learned_tagを持つとき","で区切って返す' do
-      daily_report = FactoryBot.build :daily_report, learned_tags: [
-        FactoryBot.build(:learned_tag, name: 'foo'),
-        FactoryBot.build(:learned_tag, name: 'bar')
+      daily_report = build :daily_report, learned_tags: [
+        build(:learned_tag, name: 'foo'),
+        build(:learned_tag, name: 'bar')
       ]
       expect(daily_report.learned_tags_str).to eq 'foo,bar'
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe User, type: :model do
   describe 'last_nameのバリデーション' do
     context '適切な値のとき' do
       it '有効' do
-        user = FactoryBot.build :user, last_name: 'last_name'
+        user = build :user, last_name: 'last_name'
         expect(user).to be_valid
       end
     end
 
     context 'last_nameがnilのとき' do
       it '無効' do
-        user = FactoryBot.build :user, last_name: nil
+        user = build :user, last_name: nil
         expect(user).not_to be_valid
       end
 
       it 'エラーメッセージが"を入力してください"になる' do
-        user = FactoryBot.build :user, last_name: nil
+        user = build :user, last_name: nil
         user.valid?
         expect(user.errors[:last_name]).to include('を入力してください')
       end
@@ -24,12 +24,12 @@ RSpec.describe User, type: :model do
 
     context 'last_nameが空文字のとき' do
       it '無効' do
-        user = FactoryBot.build :user, last_name: ''
+        user = build :user, last_name: ''
         expect(user).not_to be_valid
       end
 
       it 'エラーメッセージが"を入力してください"になる' do
-        user = FactoryBot.build :user, last_name: ''
+        user = build :user, last_name: ''
         user.valid?
         expect(user.errors[:last_name]).to include('を入力してください')
       end
@@ -39,19 +39,19 @@ RSpec.describe User, type: :model do
   describe 'first_nameのバリデーション' do
     context '適切な値のとき' do
       it '有効' do
-        user = FactoryBot.build :user, first_name: 'first_name'
+        user = build :user, first_name: 'first_name'
         expect(user).to be_valid
       end
     end
 
     context 'first_nameがnilのとき' do
       it '無効' do
-        user = FactoryBot.build :user, first_name: nil
+        user = build :user, first_name: nil
         expect(user).not_to be_valid
       end
 
       it 'エラーメッセージが"を入力してください"になる' do
-        user = FactoryBot.build :user, first_name: nil
+        user = build :user, first_name: nil
         user.valid?
         expect(user.errors[:first_name]).to include('を入力してください')
       end
@@ -59,12 +59,12 @@ RSpec.describe User, type: :model do
 
     context 'first_nameが空文字のとき' do
       it '無効' do
-        user = FactoryBot.build :user, first_name: ''
+        user = build :user, first_name: ''
         expect(user).not_to be_valid
       end
 
       it 'エラーメッセージが"を入力してください"になる' do
-        user = FactoryBot.build :user, first_name: ''
+        user = build :user, first_name: ''
         user.valid?
         expect(user.errors[:first_name]).to include('を入力してください')
       end
@@ -73,45 +73,45 @@ RSpec.describe User, type: :model do
 
   describe '#retired?' do
     it '在職のユーザーだったらfalse' do
-      user = FactoryBot.build :user, active: true
+      user = build :user, active: true
       expect(user.retired?).to be false
     end
 
     it '退職済みのユーザーだったらtrue' do
-      user = FactoryBot.build :user, active: false
+      user = build :user, active: false
       expect(user.retired?).to be true
     end
   end
 
   describe '#status_text' do
     it '在職のユーザーだったら"在職"' do
-      user = FactoryBot.build :user, active: true
+      user = build :user, active: true
       expect(user.status_text).to eq '在職'
     end
 
     it '退職済みのユーザーだったら"退職済み"' do
-      user = FactoryBot.build :user, active: false
+      user = build :user, active: false
       expect(user.status_text).to eq '退職済み'
     end
   end
 
   describe '#full_name' do
     it 'last_nameとfirst_nameをスペース区切りで返す' do
-      user = FactoryBot.build :user, last_name: 'full', first_name: 'name'
+      user = build :user, last_name: 'full', first_name: 'name'
       expect(user.full_name).to eq 'full name'
     end
   end
 
   describe '#can_thumbs_up?(daily_report)' do
     it 'いいねしていない場合はtrue' do
-      user = FactoryBot.build :user
-      daily_report = FactoryBot.build :daily_report
+      user = build :user
+      daily_report = build :daily_report
       expect(user.can_thumbs_up?(daily_report)).to be true
     end
 
     it 'いいねしている場合はfalse' do
-      user = FactoryBot.build :user
-      daily_report = FactoryBot.build :daily_report
+      user = build :user
+      daily_report = build :daily_report
       user.like_daily_reports << daily_report
       expect(user.can_thumbs_up?(daily_report)).to be false
     end
