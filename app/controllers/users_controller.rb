54479a9c7360a8d719291f49c5a991e6628d8a84
shunsuke-luxiar <shunsuke.tsukagoshi@luxiar.com>
2024-07-19 16:35:15 +0900

    許可するパラメーターの修正、サイト名、index.htmlの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :admin, :active)
+    params.require(:user).permit(:first_name, :last_name, :email, :admin, :active, :password, :password_confirmation)
   end
 end
